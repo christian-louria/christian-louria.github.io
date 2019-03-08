bf22a60da94c7c8627a7f7565f7abff57f9edc76
Chrisian Louria <tianlouria@gmail.com>
2019-03-08 17:18:15 -0500

    added config

--- hacks/chargedup/Config.js
+++ hacks/chargedup/Config.js
@@ -3,7 +3,4 @@ Configurations
 Credits: Original code by Dacal & Junesiphone. Modified by Evelyn.
 */
 
-var Clock = "12h";  // choose between "12h" or "24h"
-var Lang = "en";   // choose between "en", "ca", "fr", "de", "it", "ru", "pl", "pt", "cz", "no", "nl", "fi", "cn", "zh"
-
-var IconSet = "flat_white";   // choose your weather icon pack here
+var Color = "white";  // choose a color
